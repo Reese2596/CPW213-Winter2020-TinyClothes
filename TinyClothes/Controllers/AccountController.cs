82f4582127f62751509e5245e37593a43559d034
Reese2596 <reese2596@student.cptc.edu>
2020-02-13 10:52:21 -0800

    Registration fully implemented

--- TinyClothes/Controllers/AccountController.cs
+++ TinyClothes/Controllers/AccountController.cs
@@ -3,15 +3,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TinyClothes.Data;
+using TinyClothes.Models;
 
 namespace TinyClothes.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly StoreContext _context;
+
+        public AccountController(StoreContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel reg)
+        {
+            if (ModelState.IsValid)
+            {
+                //Check Username is not taken
+                if (!await AccountDb.IsUserNameTaken(reg.UserName, _context))
+                {
+                    Account acc = new Account()
+                    {
+                        FullName = reg.FullName,
+                        Email = reg.Email,
+                        Username = reg.UserName,
+                        Password = reg.Password
+                    };
+                    //add account to database
+                    await AccountDb.Register(acc, _context);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    //Display Error with other username error msg.
+                    ModelState.AddModelError(nameof(Account.Username), "Username is already taken, Please pick another.");
+                }
+            }
+            return View(reg);
         }
     }
 }
---
+++ TinyClothes/Data/AccountDb.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using TinyClothes.Data;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TinyClothes.Models;
+
+namespace TinyClothes
+{
+    public static class AccountDb
+    {
+        /// <summary>
+        /// Checks if the Username is taken or if it is available.
+        /// </summary>
+        /// <param name="userName">Username to check</param>
+        /// <param name="context">The DB context</param>
+        public static async Task<bool> IsUserNameTaken(string userName, StoreContext context)
+        {
+            bool isTaken = await (from acc in context.Accounts
+                                where acc.Username == userName
+                                select acc).AnyAsync();
+            return isTaken;
+        }
+
+        /// <summary>
+        /// Adds a single Account to the Database.
+        /// </summary>
+        /// <param name="acc">Account object</param>
+        /// <param name="context">DB Context</param>
+        public static async Task<Account> Register(Account acc, StoreContext context)
+        {
+            await context.Accounts.AddAsync(acc);
+            await context.SaveChangesAsync();
+            return acc;
+        }
+        
+    }
+}
