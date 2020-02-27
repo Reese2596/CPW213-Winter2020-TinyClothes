678f7a6f8fb39d5bc5190f03e1ffada6018de096
Makayla Reese <49567431+Reese2596@users.noreply.github.com>
2020-02-27 10:46:45 -0800

    Issue#29 shopping cart (#31)
    * Created a Cart class to add item to cart, Also created cart helper to use for cookies. Allows user to add a single item to store in cookies.
    
    * Aded Redirection to the Previous Page instead of View.
    
    * Started Adding Add to Cart Button Next to inventory items.
    
    * Add to cart now redirect to exact page user was on
    
    * Add fontAwesome and shopping cart icon
    
    * Display number of cart items
    
    * User can now add multiple cart items

---
+++ TinyClothes/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TinyClothes.Data;
+using TinyClothes.Models;
+
+namespace TinyClothes.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly StoreContext _context;
+        //To read Cookie data.
+        private readonly IHttpContextAccessor _http;
+        public CartController(StoreContext context, IHttpContextAccessor http)
+        {
+            _context = context;
+            _http = http;
+        }
+
+        /// <summary>
+        /// Display all product in cart.
+        /// </summary>
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Adds a single object to the cart.
+        /// Quantity does not matter if same object added 5 times displayed 5 times.
+        /// </summary>
+        public async Task<IActionResult> AddToCart(int id, string prevUrl)
+        {
+            //Grab the clothing Object by Id.
+            Clothing c = await ClothingDb.GetClothingByID(id, _context);
+            //Add Clothing object to be stored in cookie.
+            if(c != null)
+            {
+                CartHelper.Add(c, _http);
+            }
+            //redirect: sends you back to the url you were previously using
+            return Redirect(prevUrl);
+        }
+
+        /// <summary>
+        /// Summary/Checkout page.
+        /// </summary>
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
+    }
+}
---
+++ TinyClothes/Models/CartHelper.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TinyClothes.Models
+{
+    /// <summary>
+    /// Helper class to manage user shopping cart data using cookies.
+    /// </summary>
+    public static class CartHelper
+    {
+        /// <summary>
+        /// The Key used to access and add data to Cookie
+        /// </summary>
+        private const string CartCookie = "CartCookie";
+
+        /// <summary>
+        /// Add a single clothing object to class
+        /// </summary>
+        /// <param name="c"> A single clothing object.</param>
+        /// <param name="http"> An accessor to get to cookies.</param>
+        public static void Add(Clothing c, IHttpContextAccessor http)
+        {
+            //Grab all existing clothing items
+            List<Clothing> clothes = GetAllClothingItems(http);
+            clothes.Add(c);
+
+            //Convert the clothing object into the string representaion.
+            string data = JsonConvert.SerializeObject(clothes);
+            //Create Cookie Options 
+            CookieOptions options = new CookieOptions()
+            {
+                Expires = DateTime.Now.AddDays(14),
+                IsEssential = true,
+                Secure = true
+            };
+
+            //Create Cookie to Store string Representaion and add Cookie.
+            http.HttpContext.Response.Cookies.Append(CartCookie, data, options);
+
+        }
+
+        //Needs TO DO: refractor to use multiple inputs into cookie
+        /// <summary>
+        /// Grabs the total number of items in the cart.
+        /// Using the data stored in cookie.
+        /// </summary>
+        /// <param name="http">Used to get access to the cookies</param>
+        public static int GetCount(IHttpContextAccessor http)
+        {
+            List<Clothing> allClothes = GetAllClothingItems(http);
+            return allClothes.Count;
+        }
+
+        /// <summary>
+        /// Grab all of the clothing from cart(Cookie) and displays them in an List of type Clothing.
+        /// If no items present empty list returned. Using data stored in Cookie.
+        /// </summary>
+        /// <param name="http">Used to get access to the cookies</param>
+        public static List<Clothing> GetAllClothingItems(IHttpContextAccessor http)
+        {
+            string data = http.HttpContext.Request.Cookies[CartCookie];
+            if (string.IsNullOrWhiteSpace(data))
+            {
+                return new List<Clothing>();
+            }
+            else
+            {
+                return JsonConvert.DeserializeObject<List<Clothing>>(data);
+            }
+        }
+    }
+}
